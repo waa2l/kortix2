f70d32c64d3be11e12726f60b63ad64eb175c1ae
waa2l <lu2aorange@gmail.com>
2025-12-11 12:10:55 +0200

    Update page.tsx

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
